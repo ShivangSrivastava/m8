6996447fef2ba7db361250dc88631ad91472e385
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-05-24 16:51:42 +0530

    test: Added unit test for migration file creation

---
+++ internal/infra/fs/creater_test.go
@@ -0,0 +1,33 @@
+package fs_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/ShivangSrivastava/m8/internal/infra/fs"
+)
+
+func TestCreateMigrations(t *testing.T) {
+	dir := t.TempDir()
+
+	creator := &fs.FileCreater{dir}
+
+	version := "20250524151203"
+	name := "test_migrations"
+
+	if err := creator.CreateMigrations(version, name); err != nil {
+		t.Fatalf("CreateMigrations failed: %v", err)
+	}
+
+	upPath := filepath.Join(dir, version+"_"+name+".up.sql")
+	downPath := filepath.Join(dir, version+"_"+name+".down.sql")
+
+	if _, err := os.Stat(upPath); os.IsNotExist(err) {
+		t.Errorf("Expected file %x not found", upPath)
+	}
+
+	if _, err := os.Stat(downPath); os.IsNotExist(err) {
+		t.Errorf("Expected file %x not found", downPath)
+	}
+}
