c86aecfe07d59bf441a8de0f850fadcb3e6ac44f
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-05-25 00:23:00 +0530

    refactor: Replaced error message to better error messages

--- internal/cli/make.go
+++ internal/cli/make.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/ShivangSrivastava/m8/internal/app"
@@ -17,8 +18,10 @@ func Make(cmd *cobra.Command, args []string) error {
 		Name:    args[0],
 		Creator: creator,
 	}
+	fmt.Println("Creating migration files...")
 	if err := service.CreateFile(); err != nil {
-		log.Fatalln(err)
+		log.Fatalln("Error creating migration files:", err)
 	}
+	fmt.Println("Migration files created successfully.")
 	return nil
 }
